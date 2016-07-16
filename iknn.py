78e6b7712bc1d81bdfbf9c7ec47f852276624d40
roy7wt <roy7wt@163.com>
2016-07-16 20:37:48 +0800

    compute UB and similarity and complete while-loop

--- iknn.py
+++ iknn.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import sys
 import math
 from rtree import index
 from functions import *
@@ -9,35 +10,53 @@ from functions import *
 def iknn(query_points, time_limit, k_threshold):
     # Variables in dissertation
     candidate_set_c = []
-    LB = []  # compute LB[] for all trajectories in candidate_set_c
     lambda_threshold = k_threshold
+
+    # {trajectory:lower-bound similarity}
+    # compute LB[] for all trajectories in candidate_set_c
+    LB = {}
 
     list_lambda_points = {}  # for quick access to λ-NN(q_i)
     list_candidates = {}  # for quick access to C_i
 
-    key_count = 0
-    for point in query_points:
+    round_count = 0
+    while True:
 
-        # line 7-8
-        lambda_i_points, candidate_i = knn(point, time_limit, lambda_threshold=lambda_threshold)
+        key_count = 0
+        for point in query_points:
 
-        list_candidates[key_count] = candidate_i  # FIXME:这个地方的轨迹可能会比较少 需要在knn里面改进一下? 不然有些轨迹找不到
-        list_lambda_points[key_count] = lambda_i_points
-        key_count += 1
+            # line 7-8
+            lambda_i_points, candidate_i = knn(point, time_limit, lambda_threshold=lambda_threshold)
 
-        # line 9
-        for trajectory in candidate_i:
-            candidate_set_c.append(trajectory)
+            list_candidates[key_count] = candidate_i  # FIXME:这个地方的轨迹可能会比较少 需要在knn里面改进一下? 不然有些轨迹找不到
+            list_lambda_points[key_count] = lambda_i_points
+            key_count += 1
 
-    # print list_candidates
-    if len(candidate_set_c) > k_threshold:
-        candidate_set_c = {}.fromkeys(candidate_set_c).keys()
+            # line 9
+            for trajectory in candidate_i:
+                candidate_set_c.append(trajectory)
 
-        for trajectory in candidate_set_c:
-            print compute_lb(trajectory, query_points, list_candidates, list_lambda_points), trajectory
-            LB.append(compute_lb(trajectory, query_points, list_candidates, list_lambda_points))
+        print len(candidate_set_c)
+        if len(candidate_set_c) > k_threshold:
+            candidate_set_c = {}.fromkeys(candidate_set_c).keys()
 
-            # print list_candidates
+            # Compute LB[] for all trajectories in C
+            for trajectory in candidate_set_c:
+                LB[trajectory] = compute_lb(trajectory, query_points, list_candidates, list_lambda_points)
+
+            # k-LB[] = LB[].topK() and store trajectories
+            k_LB = list(sorted(LB, key=LB.__getitem__, reverse=True))[0:k_threshold]
+
+            # Compute UB_{n}
+            UB_n = compute_ubn(query_points, list_lambda_points)
+
+            # TODO 测试一下相似度
+            print 'sim', sim(query_points, 'data/user/P006000300000006/2014-07-22 r35.txt')
+            if LB[k_LB[-1]] > UB_n:
+                # compute UB for each candidate in C and sort candidate in C by UB in descending order
+                return refine(candidate_set_c, query_points, list_candidates, list_lambda_points, k_threshold)
+        round_count += 1
+        lambda_threshold += k_threshold * pow(2, round_count)
 
 
 # Return λ-NN(q) & λ.top trajectories scanned by λ-NN(q)
@@ -100,6 +119,26 @@ def knn(point, time_limit, lambda_threshold=10):
     return lambda_nn, {}.fromkeys(scanned_trajectories).keys()
 
 
+# Compute similarity Sim(Q,R) between Q and R
+def sim(query_points, trajectory):
+    # The points in trajectory
+    points_in_trajectory = []
+    with open(trajectory, 'r') as f:
+        for line in f:
+            temp = line.strip('\n').split('\t')
+            points_in_trajectory.append([float(temp[0]), float(temp[1])])
+
+    ret_sum = 0
+    for query_point in query_points:
+
+        temp_min = sys.float_info.max
+        for point_j in points_in_trajectory:
+            temp_min = min(temp_min,  distance_q_p(query_point, point_j))
+        ret_sum += pow(math.e, (-1) * temp_min)
+
+    return ret_sum
+
+
 # Compute LB of similarity for each candidate R_x
 def compute_lb(trajectory, query_points, list_candidates, list_lambda_points):
     # The points in trajectory
@@ -109,7 +148,7 @@ def compute_lb(trajectory, query_points, list_candidates, list_lambda_points):
             temp = line.strip('\n').split('\t')
             points_in_trajectory.append([float(temp[0]), float(temp[1])])
 
-    ret_sum = 0
+    ret_lb = 0
 
     # i \in [1:m]
     for i in range(0, len(list_candidates)):
@@ -118,8 +157,67 @@ def compute_lb(trajectory, query_points, list_candidates, list_lambda_points):
             for point in list_lambda_points.get(i):
                 if point in points_in_trajectory:  # j \in [1:λ] and p^{j}_{i} \in R_{x}
                     temp_max = max(temp_max, pow(math.e, (-1) * distance_q_p(query_points[i], point)))
-            ret_sum += temp_max
-    return ret_sum
+            ret_lb += temp_max
+    return ret_lb
+
+
+# Further define an upper bound UB of similarity for candidate trajectories in C only
+def compute_ub(trajectory, query_points, list_candidates, list_lambda_points):
+    # The points in trajectory
+    points_in_trajectory = []
+    with open(trajectory, 'r') as f:
+        for line in f:
+            temp = line.strip('\n').split('\t')
+            points_in_trajectory.append([float(temp[0]), float(temp[1])])
+
+    ret_lb_former, ret_lb_latter = 0, 0
+
+    # i \in [1:m]
+    for i in range(0, len(list_candidates)):
+        if trajectory in list_candidates.get(i):  # R_{x} \in C_{i}
+            temp_max, j = 0, 0
+            for point in list_lambda_points.get(i):
+                if point in points_in_trajectory:  # j \in [1:λ] and p^{j}_{i} \in R_{x}
+                    temp_max = max(temp_max, pow(math.e, (-1) * distance_q_p(query_points[i], point)))
+            ret_lb_former += temp_max
+        else:
+            ret_lb_latter += pow(math.e, (-1) * distance_q_p(query_points[i], list_lambda_points.get(i)[-1]))
+
+    return ret_lb_former + ret_lb_latter
+
+
+# Compute UB_{n} of similarity of all the non-scanned trajectories
+def compute_ubn(query_points, list_lambda_points):
+    ret_ubn, i = 0, 0
+    for query_point in query_points:
+        ret_ubn += pow(math.e, (-1) * distance_q_p(query_point, list_lambda_points.get(i)[-1]))
+        i += 1
+
+    return ret_ubn
+
+
+# refinement procedure is triggered to examine the exact similarity of candidate trajectories are returned
+def refine(candidate_set_c, query_points, list_candidates, list_lambda_points, k_threshold):
+    ub = {}
+    for trajectory in candidate_set_c:
+        ub[trajectory] = compute_ub(trajectory, query_points, list_candidates, list_lambda_points)
+
+    # k-best connected trajectories
+    k_BCT = {}
+    candidate = sorted(ub.items(), key=lambda t: t[1], reverse=True)
+    for x in range(0, len(candidate)):
+        sim_Rx = sim(query_points, candidate[x][0])
+        if x <= k_threshold:
+            k_BCT[candidate[x][0]] = sim_Rx
+        else:
+            temp_k_BCT = sorted(k_BCT.items(), key=lambda t: t[1])
+            min_trajectory, k_BCT_min = temp_k_BCT[0]
+            if sim_Rx > k_BCT_min:
+                del k_BCT[min_trajectory]
+                k_BCT[candidate[x][0]] = sim_Rx
+            if x == len(candidate) - 1 or k_BCT_min >= compute_ub(candidate[x + 1][0], query_points, list_candidates,
+                                                                  list_lambda_points):
+                return sorted(k_BCT.items(), key=lambda t: t[1], reverse=True)
 
 
 # Return all the users under default directory 'data/user/
@@ -162,8 +260,8 @@ def res(filename):
             fw.write('[%s,%s],\n' % (temp[0], temp[1]))
 
 
-qps = [[121.416769, 31.132241], [121.675635, 31.195697], [121.71546, 31.205094]]
-iknn(query_points=qps, time_limit='2014-07-05', k_threshold=10)
+qps = [[121.308538, 31.278788], [121.527922, 31.382309], [121.630575, 31.336281]]
+print iknn(query_points=qps, time_limit='2014-07-22', k_threshold=15)
 # query_point = [121.424815, 31.041945]
 # print knn(query_point, time_limit='2014-07-02', lambda_threshold=10)
 # print len(str(time_transform_to_int('2014-07-04 23:09:20')))
